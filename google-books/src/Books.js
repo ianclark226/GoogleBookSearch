92975a6acb778cf1add67b58aefc0fa88e6081a4
Ian Clark <ianclark226@gmail.com>
2020-04-19 10:55:56 -0400

    4/19

---
+++ google-books/src/Books.js
@@ -0,0 +1,37 @@
+import React, {Component } from 'react';
+import SearchArea from './SearchArea';
+import request from 'superagent';
+
+class Books extends Component {
+    constructor(props) {
+        super(props);
+            this.state = {
+                books: [],
+                searchField: ''
+            
+        }
+    }
+
+    searchBook = () => {
+        e.preventDefault();
+        request
+        .get("http://www.googleapis.com/books/v1/volumes")
+        .query({ q: this.searchField })
+        .then((data) => {
+            console.log(data);
+        })
+    }
+
+    handleSearch = (e) => {
+        this.setState({ searchField: e.target.value })
+    }
+    render() {
+        return (
+            <div>
+                <SearchArea searchBook={this.searchBook} handleSearch={this.handleSearch} />
+            </div>
+        );
+    }
+}
+
+export default Books;
