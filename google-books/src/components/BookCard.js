ce36d493acca83bb9e2383e787e6c320e4af3cab
Ian Clark <ianclark226@gmail.com>
2020-04-20 18:16:12 -0400

    4/20

--- google-books/src/components/BookCard.js
+++ google-books/src/components/BookCard.js
@@ -5,7 +5,7 @@ import React from 'react';
 const BookCard = (props) => {
     const { volumeInfo } = props.info;
     const {title, authors, subtitle, publishedDate} = props.info.volumeInfo;
-    const thumbNail = volumeInfo.hasOwnProperty('imageLinks') === false ? "https://vignette.wikia.nocookie.net/pandorahearts/images/a/ad/Not_available.jpg/revision/latest?cb=20141028171337" : volumeInfo.imageLinks.thumbnail;
+    const thumbNail = volumeInfo.hasOwnProperty('imageLinks') === false ? "https://upload.wikimedia.org/wikipedia/commons/6/64/Poster_not_available.jpg" : volumeInfo.imageLinks.thumbnail;
     const publishYear = volumeInfo.hasOwnProperty('publishedDate') === false ? volumeInfo['publishedDate'] = "0000" : volumeInfo.publishedDate;
 
     return (
