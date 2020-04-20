996d2b6d0dc2d96c709968b8c582d8cb9a35eb9d
Ian Clark <ianclark226@gmail.com>
2020-04-20 14:31:54 -0400

    4/20

--- google-books/src/SearchArea.js
+++ google-books/src/SearchArea.js
@@ -6,6 +6,12 @@ const SearchArea = (props) => {
             <form onSubmit={props.SearchBook} action="">
                 <input onChange={props.handleSearch} type="text"/>
                 <button type="submit">Search</button>
+                <select defaultValue="Sort" onchange={props.handleSort}>
+                    <option disabled value="Sort">Sort</option>
+                    <option disabled value="Newest">Newest</option>
+                    <option disabled value="Oldest">Oldest</option>
+                    
+                </select>
             </form>
         </div>
     )
--- google-books/src/components/Books.js
+++ google-books/src/components/Books.js
@@ -9,7 +9,8 @@ class Books extends Component {
         super(props);
             this.state = {
                 books: [],
-                searchField: ''
+                searchField: '',
+                sort: ''
             
         }
     }
@@ -20,6 +21,7 @@ class Books extends Component {
         .get("http://www.googleapis.com/books/v1/volumes")
         .query({ q: this.searchField })
         .then((data) => {
+            const cleanData = this.cleanData(data)
             this.setState({ books: [...data.body.items ]})
         })
     }
@@ -27,11 +29,42 @@ class Books extends Component {
     handleSearch = (e) => {
         this.setState({ searchField: e.target.value })
     }
+
+    handleSort = (e) => {
+        console.log(e.taget.value);
+        this.setState({ sort: e.target.value})
+    }
+
+    cleanData = (e) => {
+        const cleanData = data.body.items.map((book) => {
+            if(book.volumeInfo.hasOwnProperty('publishedDate') === false) {
+            book.volumeInfo['publishedDate'] = '0000';
+            }
+            else if (book.volumeInfo.hasOwnProperty('imageLinks')  ===false) {
+                book.volumeInfo['imageLinks'] = {thumbnail};
+
+            }
+
+            return book;
+        
+
+        })
+
+        return cleanData;
+    }
     render() {
+        const sortedBooks = this.state.books.sort((a,b) => {
+            if(this.state.sort === 'Newest') {
+                return parseInt(b.volumeInfo.publishedDate.substring(0,4)) - (a.volumeInfo.publishedDate.substring(0,4))
+            }
+            if(this.state.sort === 'Oldest') {
+                return parseInt(b.volumeInfo.publishedDate.substring(0,4)) - (a.volumeInfo.publishedDate.substring(0,4))
+            }
+        })
         return (
             <div>
-                <SearchArea searchBook={this.searchBook} handleSearch={this.handleSearch} />
-                <BookList books={this.state.books}/>
+                <SearchArea searchBook={this.searchBook} handleSearch={this.handleSearch} handleSort={this.handleSort} />
+                <BookList books={sortedBooks}/>
             </div>
         );
     }
