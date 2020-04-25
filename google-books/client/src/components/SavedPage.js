c07853c3f88b11d00fd48160ae45e237e21411dc
Ian Clark <ianclark226@gmail.com>
2020-04-25 13:45:21 -0400

    4/25

--- google-books/client/src/components/SavedPage.js
+++ google-books/client/src/components/SavedPage.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import Hero from '../components/Hero';
+import React, { Component }from 'react';
+
 import "./styles.css";
+import API from "../utilities/API";
+import Results from "../components/Results";
 
 
-function SavedPage(props) {
+class SavedPage extends Component {
+    state = {
+        savedBooks: [],
+    }
 
+    componentDidMount() {
+        API.savedBooks()
+        .then(savedBooks => this.setState({ savedBooks: savedBooks }))
+        .catch(err => console.error(err))
+    }
+
+    render() {
     return (
-    <div>
-        <Hero title={props.title} />
+    <div className="container">
+        <h2>Saved Books!</h2>
+        <Results books={this.state.savedBooks} />
+        
 
     </div>
     );
+}
 }
 
 export default SavedPage;
