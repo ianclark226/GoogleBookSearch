51642450d0d8581d4d3778ad2364100e73de4905
Ian Clark <ianclark226@gmail.com>
2020-04-28 18:30:15 -0400

    heroku

---
+++ client/build/precache-manifest.617eeefb7705b9daf175d1e2ebd31660.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8bfe5e409b70f879bc6a756c6d517b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f07b7f2b754484f6b4b4",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "49e9aead9ff3ebc0f281",
+    "url": "/static/css/main.b30058e5.chunk.css"
+  },
+  {
+    "revision": "f07b7f2b754484f6b4b4",
+    "url": "/static/js/2.d89307c4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d89307c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e9aead9ff3ebc0f281",
+    "url": "/static/js/main.288b031e.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
